fd5b713a85c8d3aab1f57aeca4537e9a1fe03f12
qyli <qiaoyili@buaa.edu.cn>
2023-10-08 14:39:05 +0800

    exclusive

--- src/generateScript.jl
+++ src/generateScript.jl
@@ -20,20 +20,22 @@ function generateScript(
      nthreads_mkl::Int64 = get(kwargs, :nthreads_mkl, 1)
      heap_size_hint::Int64 = get(kwargs, :heap_size_hint, round(0.8*mem))
      project::String = get(kwargs, :project, pwd())
+     exclusive::Bool = false
 
      file = open(filename, "w+")
 
      # slurm parameters
      println(file, "#!/bin/bash")
-     println(file, "SBATCH --job-name=$(jobname)")
-     println(file, "SBATCH --partition=$(partition)")
-     println(file, "SBATCH --mem=$(mem)gb")
-     println(file, "SBATCH --nodes=$(nodes)")
-     println(file, "SBATCH --ntasks-per-node=$(tpn)")
-     println(file, "SBATCH --cpus-per-task=$(nthreads_slurm)")
-     println(file, "SBATCH --time=$(maxtime)")
+     println(file, "#SBATCH --job-name=$(jobname)")
+     println(file, "#SBATCH --partition=$(partition)")
+     println(file, "#SBATCH --mem=$(mem)gb")
+     println(file, "#SBATCH --nodes=$(nodes)")
+     println(file, "#SBATCH --ntasks-per-node=$(tpn)")
+     println(file, "#SBATCH --cpus-per-task=$(nthreads_slurm)")
+     println(file, "#SBATCH --time=$(maxtime)")
      !isnothing(exclude) && println(file, "SBATCH --exclude=$(exclude)")
-     println(file, "SBATCH --output=$(logname)")
+     exclusive && println(file, "#SBATCH --exclusive")
+     println(file, "#SBATCH --output=$(logname)")
 
      # julia script
      print(file, "MKL_NUM_THREADS=$(nthreads_mkl) ") # set MKL nthreads
