9825af4aa743478570e8cee0ac7659dce3791c76
qyli <qiaoyili@buaa.edu.cn>
2023-10-08 14:31:39 +0800

    fix bugs

--- src/generateScript.jl
+++ src/generateScript.jl
@@ -16,7 +16,7 @@ function generateScript(
      nodes::Int64 = get(kwargs, :nodes, 1)
      tpn::Int64 = get(kwargs, :tpn, 1)
      exclude::Union{String, Nothing} = get(kwargs, :exclude, nothing)
-     logname::String = get(kwargs, :logname, filename[a+1:end-3] * ".log") 
+     logname::String = get(kwargs, :logname, filename[1:end-3] * ".log") 
      nthreads_mkl::Int64 = get(kwargs, :nthreads_mkl, 1)
      heap_size_hint::Int64 = get(kwargs, :heap_size_hint, round(0.8*mem))
      project::String = get(kwargs, :project, pwd())
@@ -25,7 +25,7 @@ function generateScript(
 
      # slurm parameters
      println(file, "#!/bin/bash")
-     println(file, "SBATCH --job-name=$(jobname)", Name)
+     println(file, "SBATCH --job-name=$(jobname)")
      println(file, "SBATCH --partition=$(partition)")
      println(file, "SBATCH --mem=$(mem)gb")
      println(file, "SBATCH --nodes=$(nodes)")
