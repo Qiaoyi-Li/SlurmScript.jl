5f09d7f05f301cd5a8509455f9152d52b0fe67ee
qyli <qiaoyili@buaa.edu.cn>
2025-03-26 19:21:11 +0800

    no requeue option

--- src/sbatchScript.jl
+++ src/sbatchScript.jl
@@ -1,6 +1,12 @@
-function sbatchScript(args...;kwargs...) 
+function sbatchScript(args...; kwargs...)
 
-     filename = generateScript(args...;kwargs...)
-     run(`sbatch $(filename)`)
-     return nothing
+	filename = generateScript(args...; kwargs...)
+	requeue::Bool = get(kwargs, :requeue, false)
+
+	if !requeue
+          run(`sbatch --no-requeue $(filename)`)
+	else
+		run(`sbatch $(filename)`)
+	end
+	return nothing
 end
